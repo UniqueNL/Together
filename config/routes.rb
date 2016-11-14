1ce60910a0772478ba601d1e4a09f710535e5169
Sam Taal <Sam@Sams-Macbook.local>
2016-11-14 16:51:33 +0100

    Added: dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :dashboard
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'dashboard#index'
 end
