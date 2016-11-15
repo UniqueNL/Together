65fa3bb46bda807ebc9d0b5187ebb4708cc2f2a7
Sam Taal <Sam@Sams-Macbook.local>
2016-11-15 09:49:04 +0100

    Fixed: chatroom

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -9,10 +9,12 @@ class ChatRoomsController < ApplicationController
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @message = Message.new
+    @chat_rooms = ChatRoom.all
   end
 
   def create
-    @chat_room = current_user.chat_rooms.build(chat_room_params)
+    @chat_room = current_user.chat_rooms.build(chat_room_params) unless current_user.blank?
     if @chat_room.save
       flash[:success] = 'Chat room added!'
       redirect_to chat_rooms_path
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'chat_rooms_controller/index'
+
   resources :chat_rooms, only: [:new, :create, :show, :index]
+
   root 'chat_rooms#index'
 
-  mount ActionCable.server => '/cable'
+  authenticate :user do
+    mount ActionCable.server => '/cable'
+  end
 end
