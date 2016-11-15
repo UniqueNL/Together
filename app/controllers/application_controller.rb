6d151a84d898a899f2d8cdcaee928192d86bf978
Sam Taal <Sam@Sams-Macbook.local>
2016-11-15 13:36:34 +0100

    Attempt to fix list bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def index
+    @chat_rooms = ChatRoom.all
+  end
 end
