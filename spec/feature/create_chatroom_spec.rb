c8b5d69e47f7fed2321fe4ec5ec0effba20d2c43
Sam Taal <Sam@Sams-Macbook.local>
2016-11-17 14:52:53 +0100

    Fixed: authentication for new chatroom

--- spec/feature/create_chatroom_spec.rb
+++ spec/feature/create_chatroom_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 require 'capybara'
 
 feature 'Create a chatroom', js: true do
-  let(:user1) { create :user, email: "admin@test.com", password: "secret", admin: true }
-  let(:user2) { create :user, email: "admin@test.com", password: "secret" }
+  let(:user) { create :user, email: "admin@test.com", password: "secret", admin: true }
 
   before { login_as user }
 
@@ -20,18 +19,17 @@ feature 'Create a chatroom', js: true do
     # Expect the new task to be displayed in the list of tasks
     expect(page).to have_content('thema1')
   end
+end
 
-  scenario 'is not possible for non-admin' do
+feature 'Cant create a chatroom as normal user', js: true do
+  let(:user) { create :user, email: "admin@test.com", password: "secret", admin: false }
 
+  before { login_as user }
+
+  scenario 'by filling in the form' do
     visit new_chat_room_path
     # Enter description in the text field
-
-    fill_in 'chatroom_title', with: "thema1"
-    fill_in 'chatroom_description', with: "Description"
-    # Press enter (to submit the form)
-    page.execute_script("$('form').submit()")
-
     # Expect the new task to be displayed in the list of tasks
-    expect(page).to have_content('thema1')
+    expect(page).not_to have_content('chatroom_title')
   end
 end
