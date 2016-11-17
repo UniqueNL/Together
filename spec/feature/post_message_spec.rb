c9276f218837c725b137f29fbc2047bd340727c3
Sam Taal <Sam@Sams-Macbook.local>
2016-11-17 16:40:56 +0100

    Fixed: test issue with id

--- spec/feature/post_message_spec.rb
+++ spec/feature/post_message_spec.rb
@@ -5,7 +5,7 @@ feature 'INTEGRATION TEST: Posting a message', js: true do
   let(:user) { create :user, email: "admin@test.com", password: "secret" }
   let!(:chat_room) {create :chat_room, user: user, title: "channel" }
   before { login_as user }
-  #
+
   # scenario 'posted in a chatroom' do
   #   visit chat_room_path(chat_room)
   #   sleep(1)
@@ -13,7 +13,7 @@ feature 'INTEGRATION TEST: Posting a message', js: true do
   #   # Expect the new task to be displayed in the list of tasks
   #   expect(page).to have_content(chat_room.title)
   #
-  #   fill_in 'formMessageInput', with: "testing\n"
+  #   fill_in 'message_body', with: "testing\n"
   #   sleep(1)
   #   expect(page).to have_content("testing")
   # end
