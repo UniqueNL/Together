245df26bfc8fab617e51c8fd969daac97f133e23
Sam Taal <Sam@Sams-Macbook.local>
2016-11-17 14:41:20 +0100

    Added: integration test for making chatroom

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,4 +1,6 @@
 class ChatRoomsController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
     @chat_rooms = ChatRoom.all
   end
--- spec/feature/create_chatroom_spec.rb
+++ spec/feature/create_chatroom_spec.rb
@@ -1,20 +1,37 @@
 require 'rails_helper'
+require 'capybara'
 
 feature 'Create a chatroom', js: true do
-  scenario 'Creating a chatroom' do
-    before { login_as user }
-    let(:user) { create :user, email: "admin@test.com", password: "secret", admin: true }
+  let(:user1) { create :user, email: "admin@test.com", password: "secret", admin: true }
+  let(:user2) { create :user, email: "admin@test.com", password: "secret" }
 
+  before { login_as user }
 
-    visit root_path
+  scenario 'by filling in the form' do
 
+    visit new_chat_room_path
     # Enter description in the text field
-    fill_in 'todo_title', with: 'Be Batman'
 
+    fill_in 'chatroom_title', with: "thema1"
+    fill_in 'chatroom_description', with: "Description"
     # Press enter (to submit the form)
     page.execute_script("$('form').submit()")
 
     # Expect the new task to be displayed in the list of tasks
-    expect(page).to have_content('Be Batman')
+    expect(page).to have_content('thema1')
+  end
+
+  scenario 'is not possible for non-admin' do
+
+    visit new_chat_room_path
+    # Enter description in the text field
+
+    fill_in 'chatroom_title', with: "thema1"
+    fill_in 'chatroom_description', with: "Description"
+    # Press enter (to submit the form)
+    page.execute_script("$('form').submit()")
+
+    # Expect the new task to be displayed in the list of tasks
+    expect(page).to have_content('thema1')
   end
 end
